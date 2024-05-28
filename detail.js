64269d0bb0f57fb8f6a89713915e92a6218c996b
adibaalya <164612947+adibaalya@users.noreply.github.com>
2024-05-28 19:24:05 +0800

    Add files via upload

---
+++ detail.js
@@ -0,0 +1,11 @@
+function toggleHeart(button) {
+  button.classList.toggle("liked");
+}
+document.getElementById("adopt-now").addEventListener("click", function() {
+  if (document.getElementById("agree").checked) {
+      alert("Thank you for your interest in adopting Simba, " + document.getElementById("name").value + "!");
+      document.getElementById("Adopt").style.display = "none";
+  } else {
+      alert("Please indicate that you agree to the terms and conditions.");
+  }
+});
